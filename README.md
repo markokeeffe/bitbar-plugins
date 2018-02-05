f2822cd1a66da06c103d664a995162fb7c014ebe
MarkO <mark.okeeffe@digistorm.com.au>
2018-02-05 11:27:58 +1000

    Increased time intervals

---
+++ README.md
@@ -0,0 +1,9 @@
+# Plugins for BitBar
+https://github.com/matryer/bitbar
+
+## Installation
+
+* Clone the repo `git clone https://github.com/markokeeffe/bitbar-plugins.git`.
+* Install dependencies `cd bitbar-plugins/.bitbar && composer install`.
+* Set up environment file `cp .env.example .env` and add your hodlings.
+* Make individual scripts executable e.g. `chmod +x c20.30s.php`.
--- c20.30m.php
+++ c20.30m.php
@@ -20,6 +20,14 @@ try {
     // Get USD-AUD exchange rate info from fixer
     $exchangeRateResponse = json_decode(file_get_contents('https://api.fixer.io/latest?base=USD&symbols=AUD'));
     $exchangeRate = $exchangeRateResponse->rates->AUD;
+
+    $exchangeEthTicker = json_decode(file_get_contents('https://api.bibox.com/v1/mdata?cmd=ticker&pair=C20_ETH'));
+    $exchangeEthPrice = $exchangeEthTicker->result->last;
+
+    $ethAudResponse = json_decode(file_get_contents('https://api.btcmarkets.net/market/ETH/AUD/tick'));
+    $ethAudPrice = $ethAudResponse->lastPrice;
+
+    $audPrice = number_format($exchangeEthPrice * $ethAudPrice, 2);
 } catch (\Exception $e) {
     $bb->newLine()
         ->setText('Error')
@@ -35,13 +43,17 @@ try {
 
 // Show C20 icon and NAV
 $bb->newLine()
-    ->setText($nav)
+    ->setText('$' . $audPrice . ' (NAV $' . number_format($nav * $exchangeRate, 2) . ')')
     ->setImage('iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAFM0aXcAAAABGdBTUEAALGPC/xhBQAAAS1JREFUKBWdkj1KBEEQhbs38ScVVzZVRPAGHsBs8Rpm3kEEjc0UjA3NPMReQDBYMHc1ElkRHL/X86pZ1w7EgjdV9d6rme7pTonoum5feSmgh6ArNPldhYiBjbdqqNdLT50tpIGVbfIEbJW5UCFGMSLjNIQgb8Q6puS1ajA5rASFuVEsS9oT5I4K8kSZ+OqTnwgZtDbeOxDHPybUQO6COYg4Lia6UzNXNh65f9XUOZgVpx/0J+IWV72ol1riJ9jAWV9Lfwn6QYT2gnDUwPR7K1V1gekA3APFCzgDm8u+0ksA2vIMRDxTaKd3QTjrDoxBXfOFhTfyYesL8Kvg2j6tph5wfyNT+mDwoTWcc57DP1or/vjPcUdXEJuHry+j7Xk4/OXs/79nv60mvvKnv/0NlHh3XudhrrUAAAAASUVORK5CYII=')
+    ->show();
+
+$bb->newLine()
+    ->setText('NAV: USD$' . number_format($nav, 2))
     ->show();
 
 // Calculate holdings in AUD
 $bb->newLine()
-    ->setText('Holdings: $' . number_format((HODLING * $nav) * $exchangeRate, 2))
+    ->setText('Holdings: $' . number_format(HODLING * $audPrice, 2))
     ->show();
 
 // Show distribution of cryptocurrencies in the fund
